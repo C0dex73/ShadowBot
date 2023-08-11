013a1e3099c7d56c37e724a989b6e721887b44f3
C0dex73 <c0d3x73@gmail.com>
2023-08-11 13:41:01 +0200

    STARTING BOT msg

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const loadCommands = require("./loader/loadCommands.js")
 const loadEvents = require("./loader/loadEvents.js")
 const config = require("./config.json")
 
+console.log("STARTING BOT...")
+
 bot.commands = new Discord.Collection()
 bot.login(config.token)
 loadCommands(bot)
