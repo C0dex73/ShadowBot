22aeb51840d5df69af2ed57a49a8b071bb86156f
C0dex73 <c0d3x73@gmail.com>
2023-08-11 20:59:50 +0200

    autorole command created

---
+++ commands/autorole.js
@@ -0,0 +1,29 @@
+const commands = require("../config.json").commands
+const ApplyRoleRule = require("./role").Apply
+
+module.exports = {
+    name : "autorole",
+    description : "gives you the selected role if authorized",
+    permission : null,
+    dm : false,
+    options : [{
+        type : commands.type.discord.role,
+        name : "role",
+        description : "the role to give to yourself",
+        required : true
+    },
+    {
+        type : commands.type.bool,
+        name : "remove",
+        description : "if true will remove instead of giving",
+        required : false
+    }],
+
+    async run(bot, msg, args){
+        if(args.getRole('role').comparePositionTo(bot.guilds.cache.get(msg.guild.id).roles.cache.find(r => r.name === "Server Booster")) < 0){
+            ApplyRoleRule(msg, msg.member, args.getRole('role'), args.getBoolean('remove'))
+        }else{
+            msg.reply({content : "You are not allowed to modify this role on yourself !", ephemeral : true})
+        }
+    }
+}
